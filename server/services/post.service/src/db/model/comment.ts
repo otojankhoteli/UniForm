e2f5d0160774ac75706fb9489b10cdb86f8ffa7c
Otar Jankhoteli <otojankhoteli@gmail.com>
2020-05-24 15:37:55 +0400

    add votable

---
+++ server/services/post.service/src/db/model/comment.ts
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+import {IPost} from '../../interface/Post';
+import {votable} from './votable';
+
+const comment = new mongoose.Schema({
+  ...votable,
+
+  text: {
+    type: String,
+    required: true,
+  },
+
+  userTags: [{
+    type: String,
+  }],
+
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'Post',
+  },
+},
+{timestamps: true});
+
+
+const CommentModel = mongoose.model<IPost & mongoose.Document>('Comment', comment);
+
+
+export {CommentModel};
