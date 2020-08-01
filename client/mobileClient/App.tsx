b1a6b666a6aebaca90654cd62df1357d1379a24f
tornikebubuteishvilii <tornikebubuteishvili1@gmail.com>
2020-08-01 18:37:27 +0400

    Updating the name of the app navigator

--- client/mobileClient/App.tsx
+++ client/mobileClient/App.tsx
@@ -1,14 +1,14 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import StartUpScreen from './src/screens/StartUpScreen';
-import { InitialAppState } from './src/shared/globalState/AppState';
-import { reducer } from './src/shared/globalState/Reducer';
-import { StateProvider } from './src/shared/globalState/AppContext';
+import "react-native-gesture-handler";
+import React from "react";
+import AppNavigator from "./src/shared/navigation/AppNavigator";
+import { InitialAppState } from "./src/shared/globalState/AppState";
+import { reducer } from "./src/shared/globalState/Reducer";
+import { StateProvider } from "./src/shared/globalState/AppContext";
 
 export default function App() {
   return (
     <StateProvider initialState={InitialAppState} reducer={reducer}>
-      <StartUpScreen />
+      <AppNavigator />
     </StateProvider>
   );
 }
