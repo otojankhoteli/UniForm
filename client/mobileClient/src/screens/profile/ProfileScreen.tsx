dd9af78ba20c5c8a4873dd3afd0c718f9c9151f2
tornikebubuteishvilii <tornikebubuteishvili1@gmail.com>
2020-08-01 18:34:30 +0400

    Adding empty screens for tab navigation

---
+++ client/mobileClient/src/screens/profile/ProfileScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "react-native";
+
+const ProfileScreen: React.FC = () => {
+  return (
+    <View style={{ flex: 1, backgroundColor: "rgba(255, 128, 64, 1)" }}></View>
+  );
+};
+
+export default ProfileScreen;
---
+++ client/mobileClient/src/screens/search/SearchScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "react-native";
+
+const SearchScreen: React.FC = () => {
+  return (
+    <View style={{ flex: 1, backgroundColor: "rgba(64, 128, 255, 1)" }}></View>
+  );
+};
+
+export default SearchScreen;
