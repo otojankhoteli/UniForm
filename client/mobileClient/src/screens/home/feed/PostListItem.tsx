d6dbf55b2428cb3ccfb9d4e044ecb8cb4f1d4ed1
tornikebubuteishvilii <tornikebubuteishvili1@gmail.com>
2020-08-01 22:20:53 +0400

    Format style change

--- client/mobileClient/src/screens/home/feed/PostListItem.tsx
+++ client/mobileClient/src/screens/home/feed/PostListItem.tsx
@@ -1,88 +1,127 @@
-import React, { useMemo, useEffect } from 'react';
-import { View, StyleSheet, TouchableOpacity } from 'react-native';
-import { Text } from 'react-native-elements';
-import Icon from 'react-native-vector-icons/FontAwesome5';
-import { PostViewModel } from '../../../api/posts/PostsApiModel';
-import AvatarCustom from '../../../shared/components/Avatar';
-import { MainColor } from '../../../shared/Const';
-import HorizontalLine from '../../../shared/components/HorizontalLine';
-import { getTimeFormat } from '../../../shared/Utils';
-import { extractNodesFromInputText } from '../../addPost/AddPostUtils';
-import { TextWithTags } from '../../addPost/TextWithTags';
-import { useUpvote, useDownvote } from '../../../api/posts/PostsApiHook';
-
-
+import React, { useMemo, useEffect } from "react";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
+import { Text } from "react-native-elements";
+import Icon from "react-native-vector-icons/FontAwesome5";
+import { PostViewModel } from "../../../api/posts/PostsApiModel";
+import AvatarCustom from "../../../shared/components/Avatar";
+import { MainColor } from "../../../shared/Const";
+import HorizontalLine from "../../../shared/components/HorizontalLine";
+import { getTimeFormat } from "../../../shared/Utils";
+import { extractNodesFromInputText } from "../../addPost/AddPostUtils";
+import { TextWithTags } from "../../addPost/TextWithTags";
+import { useUpvote, useDownvote } from "../../../api/posts/PostsApiHook";
 
 interface Props {
-  post: PostViewModel,
-  refresh: () => void
+  post: PostViewModel;
+  refresh: () => void;
 }
 export function PostListItem({ post, refresh }: Props) {
-  const textNodes = useMemo(() => extractNodesFromInputText(post.text), [post.text]);
-  const { post: upvote, result: upvoteResult, isError: upvoteFailed } = useUpvote(post.id);
-  const { post: downvote, result: downvoteResult, isError: downvoteFailed } = useDownvote(post.id);
+  const textNodes = useMemo(() => extractNodesFromInputText(post.text), [
+    post.text,
+  ]);
+  const {
+    post: upvote,
+    result: upvoteResult,
+    isError: upvoteFailed,
+  } = useUpvote(post.id);
+  const {
+    post: downvote,
+    result: downvoteResult,
+    isError: downvoteFailed,
+  } = useDownvote(post.id);
 
-  const upVoteIconStyle = post.isUpvoted ? styles.votedIconColor : styles.notVotedIconColor;
-  const downVoteIconStyle = post.isDownvoted ? styles.votedIconColor : styles.notVotedIconColor;
+  const upVoteIconStyle = post.isUpvoted
+    ? styles.votedIconColor
+    : styles.notVotedIconColor;
+  const downVoteIconStyle = post.isDownvoted
+    ? styles.votedIconColor
+    : styles.notVotedIconColor;
 
   useEffect(() => {
-    if ((upvoteResult && !upvoteFailed) || (downvoteResult && !downvoteFailed)) {
+    if (
+      (upvoteResult && !upvoteFailed) ||
+      (downvoteResult && !downvoteFailed)
+    ) {
       refresh();
     }
-  }, [upvoteResult, upvoteFailed, downvoteResult, downvoteFailed])
+  }, [upvoteResult, upvoteFailed, downvoteResult, downvoteFailed]);
 
   const navigateToCategoryScreen = () => {
     //
-  }
+  };
 
   const joinCategory = () => {
     //
-  }
+  };
 
-  return <View style={styles.container}>
-    <View style={styles.topSection}>
-      <AvatarCustom photoUrl={post.authorProfilePic} />
-      <View style={styles.topMiddleSection}>
-        <View style={styles.categorySection}>
-          <Text style={styles.clickableCategoryName}>u/</Text>
-          <TouchableOpacity onPress={navigateToCategoryScreen}>
-            <Text style={styles.clickableCategoryName}>
-              {post.categoryName}
+  return (
+    <View style={styles.container}>
+      <View style={styles.topSection}>
+        <AvatarCustom photoUrl={post.authorProfilePic} />
+        <View style={styles.topMiddleSection}>
+          <View style={styles.categorySection}>
+            <Text style={styles.clickableCategoryName}>u/</Text>
+            <TouchableOpacity onPress={navigateToCategoryScreen}>
+              <Text style={styles.clickableCategoryName}>
+                {post.categoryName}
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.authorSection}>
+            <Text style={styles.timePassedText}>Posted by </Text>
+            <Text style={styles.clickableAuthorName}>s/</Text>
+            <TouchableOpacity onPress={navigateToCategoryScreen}>
+              <Text style={styles.clickableAuthorName}>
+                {post.authorUsername}
+              </Text>
+            </TouchableOpacity>
+            <Text style={styles.timePassedText}>
+              {" "}
+              {getTimeFormat(new Date(), new Date(post.createdAt))}
             </Text>
-          </TouchableOpacity>
+          </View>
         </View>
-        <View style={styles.authorSection}>
-          <Text style={styles.timePassedText}>Posted by </Text>
-          <Text style={styles.clickableAuthorName}>s/</Text>
-          <TouchableOpacity onPress={navigateToCategoryScreen}>
-            <Text style={styles.clickableAuthorName}>
-              {post.authorUsername}
-            </Text>
-          </TouchableOpacity>
-          <Text style={styles.timePassedText}> {getTimeFormat(new Date(), new Date(post.createdAt))}</Text>
-        </View>
+        <Icon
+          color={post.isJoined ? "#AA061A" : "gray"}
+          size={20}
+          solid={post.isJoined}
+          style={styles.joinCategoryIcon}
+          onPress={joinCategory}
+          name="heart"
+        />
       </View>
-      <Icon color={post.isJoined ? "#AA061A" : "gray"} size={20} solid={post.isJoined} style={styles.joinCategoryIcon} onPress={joinCategory} name="heart" />
-    </View>
-    {/* <Text style={styles.postText}>{post.text}</Text> */}
-    <View style={styles.postText}>
-      <TextWithTags nodes={textNodes} />
-    </View>
-    <HorizontalLine mode="short" />
-    <View style={styles.bottomSection}>
-      <View style={styles.voteContainer}>
-        <View style={styles.voteActionContainer}>
-          <Icon size={30} onPress={() => { upvote({}) }} style={upVoteIconStyle} name="arrow-up" />
-          <Icon size={30} onPress={() => { downvote({}) }} style={downVoteIconStyle} name="arrow-down" />
-        </View>
-        <Text>{post.voteCount}</Text>
+      {/* <Text style={styles.postText}>{post.text}</Text> */}
+      <View style={styles.postText}>
+        <TextWithTags nodes={textNodes} />
       </View>
-      <Icon size={40} style={{ color: "gray" }} name="comments" />
+      <HorizontalLine mode="short" />
+      <View style={styles.bottomSection}>
+        <View style={styles.voteContainer}>
+          <View style={styles.voteActionContainer}>
+            <Icon
+              size={30}
+              onPress={() => {
+                upvote({});
+              }}
+              style={upVoteIconStyle}
+              name="arrow-up"
+            />
+            <Icon
+              size={30}
+              onPress={() => {
+                downvote({});
+              }}
+              style={downVoteIconStyle}
+              name="arrow-down"
+            />
+          </View>
+          <Text>{post.voteCount}</Text>
+        </View>
+        <Icon size={40} style={{ color: "gray" }} name="comments" />
+      </View>
     </View>
-  </View>
+  );
 }
-
-
 
 const styles = StyleSheet.create({
   authorSection: {
@@ -90,31 +129,31 @@ const styles = StyleSheet.create({
     alignSelf: "center",
     display: "flex",
     flexDirection: "row",
-    marginRight: "auto"
+    marginRight: "auto",
   },
   bottomSection: {
     alignItems: "center",
     display: "flex",
     flexDirection: "row",
     justifyContent: "center",
-    marginTop: "auto"
+    marginTop: "auto",
   },
   categorySection: {
     alignItems: "center",
     alignSelf: "center",
     display: "flex",
     flexDirection: "row",
-    marginRight: "auto"
+    marginRight: "auto",
   },
   clickableAuthorName: {
     color: MainColor,
     fontSize: 13,
-    fontWeight: "bold"
+    fontWeight: "bold",
   },
   clickableCategoryName: {
     color: MainColor,
     fontSize: 15,
-    fontWeight: "bold"
+    fontWeight: "bold",
   },
   container: {
     backgroundColor: "white",
@@ -126,33 +165,33 @@ const styles = StyleSheet.create({
   },
   joinCategoryIcon: {
     marginLeft: "auto",
-    marginRight: 10
+    marginRight: 10,
   },
   notVotedIconColor: {
-    color: "gray"
+    color: "gray",
   },
   postText: {
-    borderColor: 'red',
+    borderColor: "red",
     borderWidth: 1,
     paddingBottom: 10,
     paddingLeft: 15,
     paddingRight: 15,
-    paddingTop: 10
+    paddingTop: 10,
   },
   timePassedText: {
-    fontSize: 10
+    fontSize: 10,
   },
   topMiddleSection: {
     display: "flex",
     flexDirection: "column",
-    marginLeft: 10
+    marginLeft: 10,
   },
   topSection: {
     alignItems: "center",
     display: "flex",
     flexDirection: "row",
     justifyContent: "center",
-    marginBottom: 10
+    marginBottom: 10,
   },
   voteActionContainer: {
     display: "flex",
@@ -164,9 +203,9 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "center",
     marginLeft: 10,
-    marginRight: "auto"
+    marginRight: "auto",
   },
   votedIconColor: {
-    color: "#386083"
-  }
+    color: "#386083",
+  },
 });
