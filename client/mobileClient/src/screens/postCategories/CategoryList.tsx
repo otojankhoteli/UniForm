c7ad437bb5308ce6da699c6837d9c0ab53adc707
tornikebubuteishvilii <tornikebubuteishvili1@gmail.com>
2020-08-02 17:13:09 +0400

    Format style change

--- client/mobileClient/src/screens/postCategories/CategoryList.tsx
+++ client/mobileClient/src/screens/postCategories/CategoryList.tsx
@@ -1,22 +1,38 @@
-import React, { useState, useEffect } from 'react'
-import { ScrollView, View, Text, RefreshControl, SafeAreaView, FlatList, NativeSyntheticEvent, NativeScrollEvent } from 'react-native'
-import { CategoryViewModel } from '../../api/categories/CategoriesApiModel'
-import CategoryListItem from './CategoryListItem'
+import React, { useState, useEffect } from "react";
+import {
+  ScrollView,
+  View,
+  Text,
+  RefreshControl,
+  SafeAreaView,
+  FlatList,
+  NativeSyntheticEvent,
+  NativeScrollEvent,
+} from "react-native";
+import { CategoryViewModel } from "../../api/categories/CategoriesApiModel";
+import CategoryListItem from "./CategoryListItem";
 
 interface Props {
   isLoading: boolean;
-  categories: CategoryViewModel[],
+  categories: CategoryViewModel[];
   onSelect?: (categoryId: CategoryViewModel) => void;
   onRefresh: () => void;
   fetchNextPage: () => void;
   fetchPrevPage: () => void;
 }
-export default function CategoryList({ isLoading, categories, onSelect, onRefresh, fetchNextPage, fetchPrevPage }: Props) {
+export default function CategoryList({
+  isLoading,
+  categories,
+  onSelect,
+  onRefresh,
+  fetchNextPage,
+  fetchPrevPage,
+}: Props) {
   const [internalCategories, setInternalCategories] = useState([]);
 
   useEffect(() => {
-    setInternalCategories(category => category.concat(categories));
-  }, [categories])
+    setInternalCategories((category) => category.concat(categories));
+  }, [categories]);
 
   const onScrollEnd = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
     let { contentOffset } = e.nativeEvent;
@@ -27,16 +43,28 @@ export default function CategoryList({ isLoading, categories, onSelect, onRefres
     if (contentOffset.y > 0) {
       fetchNextPage();
     }
-  }
+  };
 
   const onRefreshInternal = () => {
     setInternalCategories([]);
     onRefresh();
-  }
+  };
 
-  return <FlatList data={internalCategories}
-    renderItem={category => (<CategoryListItem key={category.item.id} category={category.item} onCategoryClick={onSelect} />)}
-    onMomentumScrollEnd={onScrollEnd}
-    showsVerticalScrollIndicator
-    refreshControl={<RefreshControl refreshing={isLoading} onRefresh={onRefreshInternal} />} />
+  return (
+    <FlatList
+      data={internalCategories}
+      renderItem={(category) => (
+        <CategoryListItem
+          key={category.item.id}
+          category={category.item}
+          onCategoryClick={onSelect}
+        />
+      )}
+      onMomentumScrollEnd={onScrollEnd}
+      showsVerticalScrollIndicator
+      refreshControl={
+        <RefreshControl refreshing={isLoading} onRefresh={onRefreshInternal} />
+      }
+    />
+  );
 }
