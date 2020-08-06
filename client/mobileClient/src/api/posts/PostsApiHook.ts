8ebad5129ad68df6e0f7c827bb411de0a12c3456
tornikebubuteishvilii <tornikebubuteishvili1@gmail.com>
2020-08-06 18:12:36 +0400

    Format style change

--- client/mobileClient/src/api/posts/PostsApiHook.ts
+++ client/mobileClient/src/api/posts/PostsApiHook.ts
@@ -1,6 +1,20 @@
-import { usePostApiWithAuth, useGetApi, GetApiHookResult, usePostApi } from "../shared/ApiHook";
-import { CreatePostRequest, GetPostsResponse, PostViewModel } from "./PostsApiModel";
-import { CreatePostUri, GetFeedUri, DownvoteUri, UpvoteUri } from "./PostsApiUri";
+import {
+  usePostApiWithAuth,
+  useGetApi,
+  GetApiHookResult,
+  usePostApi,
+} from "../shared/ApiHook";
+import {
+  CreatePostRequest,
+  GetPostsResponse,
+  PostViewModel,
+} from "./PostsApiModel";
+import {
+  CreatePostUri,
+  GetFeedUri,
+  DownvoteUri,
+  UpvoteUri,
+} from "./PostsApiUri";
 import { EmptyRequest, EmptyResponse } from "../shared/ApiResponse";
 
 export function usePostCreate() {
@@ -11,26 +25,130 @@ export function useFeed() {
   return useGetApi<GetPostsResponse, PostViewModel>(GetFeedUri);
 }
 
-export function useMockFeed(): GetApiHookResult<GetPostsResponse, PostViewModel> {
-  const useGetApiResult= useGetApi<GetPostsResponse, PostViewModel>(GetFeedUri);
+export function useMockFeed(): GetApiHookResult<
+  GetPostsResponse,
+  PostViewModel
+> {
+  const useGetApiResult = useGetApi<GetPostsResponse, PostViewModel>(
+    GetFeedUri
+  );
 
   return {
     ...useGetApiResult,
-    result: [{ authorProfilePic: "", authorUsername: "Ako", authorId: "1", categoryName: "MACS", categoryId: "1", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley", voteCount: 10, files: [], id: "1123", isUpvoted: true, isDownvoted: false,isJoined:true },
-    { authorProfilePic: "", authorUsername: "Bubuta", authorId: "2", categoryName: "MACS", categoryId: "1", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley", voteCount: 10, files: [], id: "2123", isUpvoted: false, isDownvoted: true,isJoined:true },
-    { authorProfilePic: "", authorUsername: "Janxo", authorId: "3", categoryName: "MACS", categoryId: "1", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley", voteCount: 10, files: [], id: "3123", isUpvoted: true, isDownvoted: false,isJoined:false },
-    { authorProfilePic: "", authorUsername: "Ako", authorId: "4", categoryName: "MACS", categoryId: "1", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley", voteCount: 10, files: [], id: "4123", isUpvoted: false, isDownvoted: true,isJoined:true },
-    { authorProfilePic: "", authorUsername: "Ako", authorId: "5", categoryName: "MACS", categoryId: "1", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley", voteCount: 10, files: [], id: "5123", isUpvoted: false, isDownvoted: true,isJoined:false },
-    { authorProfilePic: "", authorUsername: "Ako", authorId: "6", categoryName: "MACS", categoryId: "1", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley", voteCount: 10, files: [], id: "6123", isUpvoted: false, isDownvoted: true,isJoined:false },
-    { authorProfilePic: "", authorUsername: "Ako", authorId: "7", categoryName: "MACS", categoryId: "1", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley", voteCount: 10, files: [], id: "7123", isUpvoted: false, isDownvoted: true,isJoined:false }]
-  }
+    result: [
+      {
+        authorProfilePic: "",
+        authorUsername: "Ako",
+        authorId: "1",
+        categoryName: "MACS",
+        categoryId: "1",
+        text:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley",
+        voteCount: 10,
+        files: [],
+        id: "1123",
+        isUpvoted: true,
+        isDownvoted: false,
+        isJoined: true,
+      },
+      {
+        authorProfilePic: "",
+        authorUsername: "Bubuta",
+        authorId: "2",
+        categoryName: "MACS",
+        categoryId: "1",
+        text:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley",
+        voteCount: 10,
+        files: [],
+        id: "2123",
+        isUpvoted: false,
+        isDownvoted: true,
+        isJoined: true,
+      },
+      {
+        authorProfilePic: "",
+        authorUsername: "Janxo",
+        authorId: "3",
+        categoryName: "MACS",
+        categoryId: "1",
+        text:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley",
+        voteCount: 10,
+        files: [],
+        id: "3123",
+        isUpvoted: true,
+        isDownvoted: false,
+        isJoined: false,
+      },
+      {
+        authorProfilePic: "",
+        authorUsername: "Ako",
+        authorId: "4",
+        categoryName: "MACS",
+        categoryId: "1",
+        text:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley",
+        voteCount: 10,
+        files: [],
+        id: "4123",
+        isUpvoted: false,
+        isDownvoted: true,
+        isJoined: true,
+      },
+      {
+        authorProfilePic: "",
+        authorUsername: "Ako",
+        authorId: "5",
+        categoryName: "MACS",
+        categoryId: "1",
+        text:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley",
+        voteCount: 10,
+        files: [],
+        id: "5123",
+        isUpvoted: false,
+        isDownvoted: true,
+        isJoined: false,
+      },
+      {
+        authorProfilePic: "",
+        authorUsername: "Ako",
+        authorId: "6",
+        categoryName: "MACS",
+        categoryId: "1",
+        text:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley",
+        voteCount: 10,
+        files: [],
+        id: "6123",
+        isUpvoted: false,
+        isDownvoted: true,
+        isJoined: false,
+      },
+      {
+        authorProfilePic: "",
+        authorUsername: "Ako",
+        authorId: "7",
+        categoryName: "MACS",
+        categoryId: "1",
+        text:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley",
+        voteCount: 10,
+        files: [],
+        id: "7123",
+        isUpvoted: false,
+        isDownvoted: true,
+        isJoined: false,
+      },
+    ],
+  };
 }
 
-
-export function useUpvote(postId:string){
+export function useUpvote(postId: string) {
   return usePostApiWithAuth<EmptyRequest, EmptyResponse>(UpvoteUri(postId));
 }
 
-export function useDownvote(postId:string){
+export function useDownvote(postId: string) {
   return usePostApiWithAuth<EmptyRequest, EmptyResponse>(DownvoteUri(postId));
 }
