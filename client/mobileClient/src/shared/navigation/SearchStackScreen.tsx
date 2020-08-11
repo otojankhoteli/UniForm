2f3de22127915aa1b478936374abef81bac3d3f4
tornikebubuteishvilii <tornikebubuteishvili1@gmail.com>
2020-08-11 10:27:43 +0400

    Adding the add category screen to nav stack

--- client/mobileClient/src/shared/navigation/SearchStackScreen.tsx
+++ client/mobileClient/src/shared/navigation/SearchStackScreen.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import SearchScreen from "../../screens/search/SearchScreen";
+import AddCategoryScreen from "../../screens/addCategory/AddCategoryScreen";
 
 const SearchStack = createStackNavigator();
 
@@ -11,6 +12,11 @@ const SearchStackScreen: React.FC = () => {
         name={"Search"}
         component={SearchScreen}
         options={{ headerTitle: "Categories" }}
+      ></SearchStack.Screen>
+      <SearchStack.Screen
+        name={"AddCategory"}
+        component={AddCategoryScreen}
+        options={{ headerTitle: "Add Category" }}
       ></SearchStack.Screen>
     </SearchStack.Navigator>
   );
