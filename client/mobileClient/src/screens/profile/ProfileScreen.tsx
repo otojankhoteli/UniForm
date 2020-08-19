26668301efcac789b5ba129de249e04dd7c35df0
tornikebubuteishvilii <tornikebubuteishvili1@gmail.com>
2020-08-19 12:47:03 +0400

    Adding placeholder user data to profile screen

--- client/mobileClient/src/screens/profile/ProfileScreen.tsx
+++ client/mobileClient/src/screens/profile/ProfileScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Image, Text } from "react-native";
 import { useGlobalState } from "../../shared/globalState/AppContext";
 import { Button } from "react-native-elements";
 
@@ -14,7 +14,28 @@ const ProfileScreen: React.FC = () => {
   };
 
   return (
-    <View style={{ flex: 1, backgroundColor: "rgba(255, 128, 64, 1)" }}>
+    <View style={{ flex: 1 }}>
+      <View style={{ flexDirection: "row", padding: 15 }}>
+        <Image
+          style={{ width: 100, height: 100 }}
+          source={{
+            uri:
+              account && account.user && account.user.photoURL
+                ? account.user.photoURL
+                : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn2.iconfinder.com%2Fdata%2Ficons%2Frcons-user%2F32%2Fmale-shadow-circle-512.png&f=1&nofb=1",
+          }}
+        ></Image>
+        <View style={{ marginLeft: 15 }}>
+          <Text style={{ fontSize: 24, fontWeight: "bold" }}>
+            {account && account.user && account.user.firstName
+              ? `${account.user.firstName} ${account.user.lastName}`
+              : "Tornike Bubuteishvili"}
+          </Text>
+          <Text style={{ fontSize: 16 }}>
+            {account && account.user ? account.user.role : "Student"}
+          </Text>
+        </View>
+      </View>
       <Button title="Logout" onPress={logout} />
     </View>
   );
