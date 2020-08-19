73acfe963cfaeffb8afafcadc377ff42548d77d4
tornikebubuteishvilii <tornikebubuteishvili1@gmail.com>
2020-08-19 12:46:26 +0400

    Adding notification screen

--- client/mobileClient/src/screens/notifications/NotificationsScreen.tsx
+++ client/mobileClient/src/screens/notifications/NotificationsScreen.tsx
@@ -1,10 +1,31 @@
 import React from "react";
 import { View } from "react-native";
+import {
+  NotificationViewModel,
+  NotificationType,
+} from "../../api/notifications/NotificationsApiModel";
+import NotificationList from "./NotificationList";
 
 const NotificationsScreen: React.FC = () => {
   return (
-    <View style={{ flex: 1, backgroundColor: "rgba(64, 255, 128, 1)" }}></View>
+    <View style={{ flex: 1 }}>
+      <NotificationList data={tempData} />
+    </View>
   );
 };
 
 export default NotificationsScreen;
+
+let tempData: NotificationViewModel[] = [];
+for (let i = 0; i < 17; i++) {
+  tempData.push({
+    fromId: "",
+    fromName: "ako" + i,
+    fromProfilePictureUri: "",
+    id: i.toString(),
+    text:
+      "did something to your something.did something to your something.did something to your something.did something to your something.did something to your something.did something to your something.did something to your something.did something to your something.did something to your something.did something to your something.",
+    type: NotificationType.Post,
+    whereId: "",
+  });
+}
