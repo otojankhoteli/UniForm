655ce0a7bb7e77012fc0dc7b20076347160c21b8
Otar Jankhoteli <otojankhoteli@gmail.com>
2020-08-20 15:54:24 +0400

    add notification service for storing and getting notifications

--- server/services/notification.service/src/service/notification.ts
+++ server/services/notification.service/src/service/notification.ts
@@ -1,23 +1,55 @@
 import {Service, Inject} from 'typedi';
 import {Document, Model} from 'mongoose';
 import NotFoundError from '../util/error/NotFoundError';
-import {INotification} from '../interface/Notification';
+import {NotificationViewModel} from '../interface/Notification';
 
 
 @Service()
 export class NotificationService {
   constructor(
     @Inject('NotificationModel')
-    private NotificationModel: Model<INotification & Document>,
+    private NotificationModel: Model<NotificationViewModel & Document>,
   ) {
   }
 
-  public async save(notification: INotification) {
+  public async save(notification: NotificationViewModel) {
     console.log('saving notification: ', notification);
     return this.NotificationModel.create(notification);
   }
 
-  public async saveBulk(notifications: INotification[]) {
+  public async saveBulk(notifications: NotificationViewModel[]) {
     return this.NotificationModel.insertMany(notifications);
   }
+
+  public async getNotificationsOf(currentUser: string, skip = 0, limit = 10) {
+    const result = await this.NotificationModel
+        .find()
+        .where('toId')
+        .equals(currentUser)
+        .skip(skip)
+        .limit(limit)
+        .sort({createdAt: 'desc'})
+        .lean();
+    return result.map<NotificationViewModel>(NotificationService.toViewModel);
+  }
+
+  private static toViewModel(e) {
+    return {
+      id: e._id,
+      type: e.type,
+      fromId: e.fromId,
+      fromName: e.fromName,
+      toId: e.toId,
+      toName: e.toName,
+      toDeviceId: e.toDeviceId,
+      whereId: e.whereId,
+      whereText: e.whereText,
+      notificationText: e.notificationText,
+      seen: e.seen,
+      etc: e.etc,
+      createdAt: e.createdAt,
+      updatedAt: e.updatedAt,
+    };
+  }
 }
+
