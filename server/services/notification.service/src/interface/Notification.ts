c683b6ec0278db8443df882e0c88dcf46e347d26
Otar Jankhoteli <otojankhoteli@gmail.com>
2020-08-20 15:58:36 +0400

    update NotificationViewModel interface

--- server/services/notification.service/src/interface/Notification.ts
+++ server/services/notification.service/src/interface/Notification.ts
@@ -5,22 +5,47 @@ export enum PostNotification {
   Tag = 'post_tag',
 }
 
+export interface NotificationViewModel {
+  id?: string,
+  type: string,
+  fromId: string,
+  fromName: string,
+  toId: string,
+  toName: string,
+  toDeviceId: string,
+  whereId: string,
+  whereText: string,
+  notificationText?: string,
+  seen?: boolean,
+  etc?: any,
+  createdAt?: Date,
+  updatedAt?: Date,
+}
 
-export interface INotification {
+interface To {
+  _id: string,
+  name: string,
+  deviceId: string,
+}
+
+interface PostServiceNotification {
   _id: string,
   type: string,
   from: {
     _id: string,
     name: string,
   },
-  to: {
-    _id: string,
-    name: string,
-    deviceId: string,
-  },
   where: {
     _id: string,
     text?: string,
   },
   etc?: any,
+}
+
+export interface SingleAddressNotification extends PostServiceNotification {
+  to: To,
+}
+
+export interface MultiAddressNotification extends PostServiceNotification {
+  to: To[],
 }
