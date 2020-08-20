b9f66d27f6735835625ea7c9b20a5863eeb4feb2
Otar Jankhoteli <otojankhoteli@gmail.com>
2020-08-20 15:54:44 +0400

    change notification model

--- server/services/notification.service/src/db/model/notification.ts
+++ server/services/notification.service/src/db/model/notification.ts
@@ -1,45 +1,40 @@
 import mongoose from 'mongoose';
-import {INotification} from '../../interface/Notification';
+import {NotificationViewModel} from '../../interface/Notification';
 
 const notification = new mongoose.Schema({
   type: {
     type: String,
   },
-
-  from: {
-    _id: {
-      type: String,
-    },
-    name: {
-      type: String,
-    },
+  fromId: {
+    type: String,
   },
-
-  to: {
-    _id: {
-      type: String,
-    },
-    name: {
-      type: String,
-    },
-    deviceId: {
-      type: String,
-    },
+  fromName: {
+    type: String,
   },
-
-  where: {
-    _id: {
-      type: String,
-    },
-    text: {
-      type: String,
-    },
+  toId: {
+    type: String,
   },
-
+  toName: {
+    type: String,
+  },
+  toDeviceId: {
+    type: String,
+  },
+  whereId: {
+    type: String,
+  },
+  whereText: {
+    type: String,
+  },
+  notificationText: {
+    type: String,
+  },
+  seen: {
+    type: Boolean,
+  },
   etc: {},
-
 },
 {timestamps: true});
 
 
-export const NotificationModel = mongoose.model<INotification & mongoose.Document>('Notification', notification);
+export const NotificationModel = mongoose.model<NotificationViewModel & mongoose.Document>('Notification', notification);
