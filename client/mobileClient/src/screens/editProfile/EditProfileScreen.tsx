e734548d6393befc186d211c5ceff0fad6543ed5
tornikebubuteishvilii <tornikebubuteishvili1@gmail.com>
2020-08-22 13:17:27 +0400

    Adding edit profile screen

---
+++ client/mobileClient/src/screens/editProfile/EditProfileScreen.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { View } from "react-native";
+
+const EditProfileScreen: React.FC = () => {
+  return <View style={{ flex: 1 }}></View>;
+};
+
+export default EditProfileScreen;
