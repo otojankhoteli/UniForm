90d012f7832367728c082fb1d49c06b885e30afa
tornikebubuteishvilii <tornikebubuteishvili1@gmail.com>
2020-08-24 23:27:36 +0400

    Empty settings screen

---
+++ client/mobileClient/src/screens/settings/SettingsScreen.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { View } from "react-native";
+
+const SettingsScreen: React.FC = () => {
+  return <View style={{ flex: 1 }}></View>;
+};
+
+export default SettingsScreen;
