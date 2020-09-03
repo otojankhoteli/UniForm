83384dcb9f83b2f9ee1c63cebd91035f45189779
Otar Jankhoteli <otojankhoteli@gmail.com>
2020-09-03 11:05:33 +0400

    add auth to comment routes

--- server/services/post.service/src/api/route/comment.ts
+++ server/services/post.service/src/api/route/comment.ts
@@ -11,14 +11,14 @@ const router = Router();
 
 router.use('/',
     pageParser,
-    // authenticate,
+    authenticate,
 );
 
 
 router.get('/', asyncMw(async (req, res, _) => {
   const commentService = Container.get(CommentService);
-  // const userId = req.currentUser._id;
-  const userId = '5ebfd7a5c2be538124b18cd7';
+  const userId = req.currentUser._id;
+  // const userId = '5ebfd7a5c2be538124b18cd7';
   res.send(await commentService.getPostComments({
     userId,
     postId: req.query.postId,
@@ -29,15 +29,15 @@ router.get('/', asyncMw(async (req, res, _) => {
 
 router.get('/:commentId', asyncMw(async (req, res, _) => {
   const commentService = Container.get(CommentService);
-  // const userId = req.currentUser._id;
-  const userId = '5ebfd7a5c2be538124b18cd7';
+  const userId = req.currentUser._id;
+  // const userId = '5ebfd7a5c2be538124b18cd7';
   res.send(await commentService.getCommentById(req.params.commentId, userId));
 }));
 
 router.post('/', asyncMw(async (req, res, _) => {
   const commentService = Container.get(CommentService);
-  // const userId = req.currentUser._id;
-  const userId = '5ebfd7a5c2be538124b18cd7';
+  const userId = req.currentUser._id;
+  // const userId = '5ebfd7a5c2be538124b18cd7';
   const comment: UpsertCommentRequest = {...req.body, authorId: userId};
   res.send(await commentService.save(comment));
 }));
@@ -55,16 +55,16 @@ router.put('/:commentId', asyncMw(async (req, res, _) => {
 
 router.post('/:commentId/_upvote', asyncMw(async (req, res, _) => {
   const commentService = Container.get(CommentService);
-  // const userId = req.currentUser._id;
-  const userId = '5ebfd7a5c2be538124b18cd7';
+  const userId = req.currentUser._id;
+  // const userId = '5ebfd7a5c2be538124b18cd7';
   await commentService.upVote(req.params.commentId, userId);
   res.sendStatus(200);
 }));
 
 router.post('/:commentId/_downvote', asyncMw(async (req, res, _) => {
   const commentService = Container.get(CommentService);
-  // const userId = req.currentUser._id;
-  const userId = '5ebfd7a5c2be538124b18cd7';
+  const userId = req.currentUser._id;
+  // const userId = '5ebfd7a5c2be538124b18cd7';
   await commentService.downVote(req.params.commentId, userId);
   res.sendStatus(200);
 }));
@@ -72,8 +72,8 @@ router.post('/:commentId/_downvote', asyncMw(async (req, res, _) => {
 router.post('/:commentId/_unreact',
     asyncMw(async (req, res, _) => {
       const commentService = Container.get(CommentService);
-      // const userId = req.currentUser._id;
-      const userId = '5ebfd7a5c2be538124b18cd7';
+      const userId = req.currentUser._id;
+      // const userId = '5ebfd7a5c2be538124b18cd7';
       await commentService.unReact(req.params.commentId, userId);
       res.sendStatus(200);
     }));
